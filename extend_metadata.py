289f33e71e3bcf761110564bea6904a2cb60f64b
Aleksandr Gorbachev <aleksandr.gorbachev@sharp-dev.net>
2024-02-13 19:55:33 +0000

    fix in import

--- extend_metadata.py
+++ extend_metadata.py
@@ -4,6 +4,7 @@ import os
 import json
 import re
 import argparse
+import datetime
 from pytube import YouTube
 
 parser = argparse.ArgumentParser(description='Learn materials metadata extender')    
