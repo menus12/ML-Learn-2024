b49c17f66d59ece86fa12da2759b7c686a9efa4a
Aleksandr Gorbachev <menus12@mail.ru>
2024-02-16 17:57:57 +0100

    cleaning and adding comments

--- extend_metadata.py
+++ extend_metadata.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# To run script do the following steps:
+# > pip3 install -f requirements.txt
+# > chmod +x ./extend_metadata.py
+# > ./extend_metadata.py -file raw_data/dev_materials.json --update True
+
 import os
 import json
 import re
@@ -7,6 +12,7 @@ import argparse
 import datetime
 from pytube import YouTube
 
+# Adding command line parameters
 parser = argparse.ArgumentParser(description='Learn materials metadata extender')    
 parser.add_argument('--file', type=str, help='Materials dump from database')
 parser.add_argument('--update', type=bool, help='Update metadata (default is False)')
@@ -19,26 +25,25 @@ if args.file == None:
     print (parser.print_help())
     exit(1)
 
-# Opening JSON file
-f = open(args.file)
- 
-# returns JSON object as  a dictionary
-source_file = json.loads(f.read())
 
-# Closing file
-f.close()
+f = open(args.file)                 # Opening JSON file
+source_file = json.loads(f.read())  # returns JSON object as  a dictionary
+f.close()                           # Closing file
 
 print ('File is ' + args.file)
 print ('---')
 
 start_time = datetime.datetime.now()
 
+# iterating over materials collection
 for material in source_file:
     c_text = ''
     if material['materialType'] == 'course' or material['materialType'] == 'learning-path':
         continue
     print ('oid: ' + material['_id']['$oid'])
     print (' |-- Material type is ' + material['materialType'])
+    
+    # extract text from test questions
     if material['materialType'] == 'test' or material['materialType'] == 'test-exam':
         t = ''
         print (' |-- Number of questions: ' + str(len(material['questions'])))
@@ -50,44 +55,41 @@ for material in source_file:
             for answer in question['answers']:
                 t += answer['answer'] + ' '
         material['text'] = t
-        #print ('Debug: ' + c_text)
+    
+    c_text = re.sub('<\!--.*-->',"", material['text'])  # remove markdown comments
+    c_text = re.sub('\\n'," ", c_text)                  # remove newliners
 
-
-    #if material['materialType'] == 'lecture':
-    c_text = re.sub('<\!--.*-->',"", material['text'])                      # remove markdown comments
-    c_text = re.sub('\\n'," ", c_text)                                      # remove newliners
-
+    # extract duration of youtube videos embeded in text    
     videos = re.findall('https:\/\/www\.youtube\.com\/watch\?v=[^\)]+', c_text)
     material['video_minutes'] = len(videos)
     if len(videos) != 0:
         for video in videos:
             yt = YouTube(video)
             video_length = yt.length
-            #print (' |-- Video ' + video + ' is ' + str(int(video_length // 60)) + ' minutes ' + str(int(video_length % 60)) + ' seconds long')
             material['video_minutes'] += round(video_length / 60)
             print (' |-- material has ' + str(material['video_minutes']) + ' video minutes.')
-    c_text = re.sub('\!\[youtube\]\(https:\/\/www\.youtube\.com\/watch\?v=[^\)]+\)'," ", c_text)
     
-    ext_links = re.findall('\[.*?\]\(http.*?\)', c_text)                      # count external links
-    material['ext_links'] = len(ext_links)
-    print (' |-- material has ' + str(material['ext_links']) + ' links.')
-    c_text = re.sub('\[.*?\]\(http.*?\)'," ", c_text)                         # remove external links from text
+    c_text = re.sub('\!\[youtube\]\(https:\/\/www\.youtube\.com\/watch\?v=[^\)]+\)'," ", c_text) # remove video links from text
+    
+    c_text = re.sub('\[.*?\]\(http.*?\)'," ", c_text)  # remove external links from text
         
-    c_text = re.sub('\[.*?\]\(.*?\.pka\)'," ", c_text)                              # remove pka assets from text
+    c_text = re.sub('\[.*?\]\(.*?\.pka\)'," ", c_text) # remove pka assets from text
 
-    p = re.search('\[.*?\]\(.*?(\.svg|\.gif|\.png|\.jpg|\.jpeg)\)', c_text)
-    pics = re.findall('\[.*?\]\(.*?(\.svg|\.gif|\.png|\.jpg|\.jpeg)\)', c_text)   # count pictures
+    # extract number of illustrations in text
+    re_pics = '\[.*?\]\(.*?(\.svg|\.gif|\.png|\.jpg|\.jpeg)\)'
+    pics = re.findall(re_pics, c_text)   # count pictures
     material['pics'] = len(pics)
     print (' |-- material has ' + str(material['pics']) + ' pics.')
-    c_text = re.sub('\[.*?\]\(.*?(\.svg|\.gif|\.png|\.jpg|\.jpeg)\)'," ", c_text) # remove external links from text
+    c_text = re.sub(re_pics," ", c_text) # remove external links from text
 
-    c_text = re.sub('[^а-яА-Яa-zA-Z0-9 -]'," ", c_text)                     # remove all non-alpha-numeric
-    c_text = re.sub('\s{2,}'," ", c_text)                                   # trim double whitespaces
+    c_text = re.sub('[^а-яА-Яa-zA-Z0-9 -]'," ", c_text)   # remove all non-alpha-numeric
+    c_text = re.sub('\s{2,}'," ", c_text)                 # trim double whitespaces
 
-    # print(c_text)
+    # counting remainig words in text
     material['words'] = len(c_text.split())
     print (' |-- material has ' + str(material['words']) + ' words.')
 
+# updating JSON with extracted features
 if args.update: 
     new_metadata = json.dumps(source_file, indent = 4, ensure_ascii=False)
     with open(args.file, 'w') as file:
